96880c25562e5c9685b3084c3b3500796abe58c0
ihsyamdev <ihsotas.yamasaki.dev@gmail.com>
2024-08-21 22:12:13 +0900

    post/createに対するパーミッションを削除

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,12 @@ Route::get('/', function () {
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth', 'verified')->group(function () {
-    Route::get('/dashboard', [PostController::class, 'index'])->middleware('auth', 'verified')->name('dashboard');
+    Route::get('/dashboard', [PostController::class, 'index'])->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('post/create', [PostController::class, 'create'])->middleware('can:test')->name('post.create');
+    // Route::get('post/create', [PostController::class, 'create'])->middleware('can:test')->name('post.create');
+    Route::get('post/create', [PostController::class, 'create'])->name('post.create');
     Route::post('post', [PostController::class, 'store'])->name('post.store');
     Route::get('post', [PostController::class, 'index']);
     Route::get('post/show/{post}', [PostController::class, 'show'])->name('post.show');
