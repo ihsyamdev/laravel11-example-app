b101b73f904fded6ae70630ea7e7bb350abf5048
ihsyamdev <ihsotas.yamasaki.dev@gmail.com>
2024-08-21 22:10:31 +0900

    メールの認証を有効化し、各ページへのアクセス制限を追加

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ Route::get('/', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', [PostController::class, 'index'])->middleware('auth')->name('dashboard');
+Route::middleware('auth', 'verified')->group(function () {
+    Route::get('/dashboard', [PostController::class, 'index'])->middleware('auth', 'verified')->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
