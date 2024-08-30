a4226424696237e46e4b247950b4a8d5325d799d
ihsyamdev <ihsotas.yamasaki.dev@gmail.com>
2024-08-30 18:21:36 +0900

    メール認証の実装を削除

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ Route::get('/', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth', 'verified')->group(function () {
+Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [PostController::class, 'index'])->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
